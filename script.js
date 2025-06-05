833e42172d587c32b5e14206f6dd5aa2cc6ce958
Seetansh <87255041+Seetansh@users.noreply.github.com>
2025-06-05 08:26:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", function () {
+  document.getElementById("status").outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+});
